2d324274d24febf1581e42930a80089793027889
Marcos <marcosantoniop47@gmail.com>
2025-06-11 20:56:26 -0300

    criei navBar e crud de acessorio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { VeiculoMarcaComponent } from './veiculo-marca/veiculo-marca.component';
 import { FuncionarioComponent } from './funcionario/funcionario.component';
+import { AcessorioComponent } from './acessorios/acessorio.component';
 
 export const routes: Routes = [
     {path: 'marcas',component:VeiculoMarcaComponent},
     {path: 'funcionarios',component:FuncionarioComponent},
+    {path: 'acessorios',component:AcessorioComponent},
 ];
