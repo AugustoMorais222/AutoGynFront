6e82206c89335eb8a8bdb9184145830b1be4a35b
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-06-14 18:02:36 -0300

    implementada tela de consulta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { VeiculoMarcaComponent } from './veiculo-marca/veiculo-marca.component';
 import { FuncionarioComponent } from './funcionario/funcionario.component';
 import { AcessorioComponent } from './acessorios/acessorio.component';
 import { ClienteComponent } from './cliente/cliente.component';
+import { ConsultaServicoComponent } from './consulta-servico/consulta-servico.component';
 
 export const routes: Routes = [
     {path: 'marcas',component:VeiculoMarcaComponent},
     {path: 'funcionarios',component:FuncionarioComponent},
     {path: 'acessorios',component:AcessorioComponent},
     {path: 'clientes',component:ClienteComponent},
+    {path: 'consulta-servico', component: ConsultaServicoComponent}
 ];
